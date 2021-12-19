6ef7ed5e89336141012edebd8164f3ab53191ca6
szotp <qwertyszot@gmail.com>
2021-12-19 11:41:58 +0100

    fix archive script

--- Scripts/archive.sh
+++ Scripts/archive.sh
@@ -1,2 +1,2 @@
 # title xcodebuild: archive
-xcodebuild clean archive -scheme PackagedAppExample -archivePath out.xcarchive
+xcodebuild clean archive -scheme PackagedAppExample -archivePath out.xcarchive -workspace PackagedAppExample.xcworkspace
